582c6598905e5337cd1296b36e6ffa6cd68a4a2b
seanald <sean_m_fitzgerald@tutanota.com>
2020-05-12 22:28:00 -0700

    megacommit

--- cards.py
+++ cards.py
@@ -1,4 +1,5 @@
-import requests, json, enum, time, textwrap
+import requests, json, time, textwrap
+from enum import Enum
 
 class State(Enum):
     LOBBY = 0
@@ -8,19 +9,38 @@ class State(Enum):
 def success(text):
     lines = text.splitlines()
     if lines.pop(0) == "SUCCESS":
-        return "\n".join(lines)
-    return None
+        return (True, "\n".join(lines))
+    return (False, "\n".join(lines))
 
+def checkIntInput(int_str, min_val, max_val):
+    try:
+        int(int_str)
+        if int(int_str) >= min_val and int(int_str) <= max_val:
+            return True
+        return False
+    except ValueError:
+        return False
 
-def qryGame(url, game_id, player_id):
+def getIntInput(prompt, min_val, max_val):
+    print(prompt)
+    i = input()
+    while checkIntInput(i, min_val, max_val) == False:
+        print("Invalid")
+        print(prompt)
+        i = input()
+    return int(i)
+
+def qryGame(url, session_id):
     data = {
-            "game_id": game_id
-            "player_id": player_id}
+            "game_id": game_id,
+            "session_id": session_id}
 
     r = requests.post(url + "/qry", data=data)
-    rlines = r.text.splitlines()
-    if rlines.pop(0) == "SUCCESS":
-        return json.loads("\n".join(rlines))
+    text = success(r.text)
+    if text[0]:
+        return json.loads(text[1])
+    print("Query failed:")
+    print(text[1])
     return None
 
 def joinGame(url, game_id, player_id, password=""):
@@ -29,8 +49,11 @@ def joinGame(url, game_id, player_id, password=""):
             "player_id": player_id,
             "password": password}
     r = requests.post(url + "/join", data=data)
-    if r.splitlines()[0] == "SUCCESS":
-        return True
+    text = success(r.text)
+    if text[0]:
+        return text[1]
+    print("Join failed:")
+    print(text[1])
     return False
 
 def login(url, nick=""):
@@ -57,54 +80,58 @@ def isGame(url, game_id):
         
         return False
 
-def createGame(url, game_name, player_id, hidden=False):
+def createGame(url, game_name, player_id, hidden=False, max_players=2):
     data={
             "game_name": game_name,
             "player_id": player_id,
-            "hidden": int(hidden)}
+            "hidden": int(hidden),
+            "password": "",
+            "max_players": max_players
+            }
 
     r = requests.post(url + "/create", data=data)
 
-    rlines = r.text.splitlines()
-    if rlines[0] == "SUCCESS":
-        return rlines[1]
+    text = success(r.text)
+    if text[0]:
+        return text[1].splitlines()
+    print("Failed to create game:")
+    print(text[1])
     return None
 
-def startGame(url, game_id, player_id):
-    data = {"game_id":game_id, "player_id":player_id}
-    r = requests.post(url + "/start", data=data)
+def startGame(url, session_id):
+    data = {"session_id":session_id, "cmd":"start", "args":"[]"}
+    r = requests.post(url + "/cmd", data=data)
     text = success(r.text)
-    if text:
+    if text[0]:
         return True
+    print("Failed to start game:")
+    print(text[1])
     return False
 
-def checkIntInput(int_str):
-    try:
-        int(int_str)
-        return True
-    except ValueError:
-        return False
-
-def getIntInput(prompt, min_val, max_val):
-    print(prompt)
-    i = input()
-    while(checkIntInput(i) == False and
-            int(i) < 1 and int(c) > max_val):
-        print("Invalid")
-        print(prompt)
-        i = input()
-    return int(i)
-
-def submit(url, game_id, player_id, sub):
+def submit(url, session_id, sub):
     sub_json = json.dumps(sub)
-    data = {"game_id":game_id, "player_id":player_id, "sub":sub_json}
-    r = requests.post(url + "/submit", data=data)
+    data = {"session_id":session_id, "cmd":"submit", "args":sub_json}
+    r = requests.post(url + "/cmd", data=data)
     text = success(r.text)
-    if text:
-        return text
+    if text[0]:
+        return text[1]
+    print("Failed to submit:")
+    print(text[1])
+    return False
+
+def select(url, session_id, sel_id):
+    sel_json = json.dumps([sel_id])
+    data = {"session_id":session_id, "cmd":"select", "args":sel_json}
+    r = requests.post(url + "/select", data=data)
+    text = success(r.text)
+    if text[0]:
+        return text[1]
+    print("Selection failed:")
+    print(text[1])
     return False
 
 def unpackCardText(text):
+    text = json.loads(text)
     text = " _____ ".join(text)
     return "\n".join(textwrap.wrap(text, width=50))
 
@@ -120,6 +147,7 @@ if __name__ == "__main__":
         exit(1)
 
     game_id = ""
+    session_id = ""
 
     print("1: Create game")
     print("2: Enter game code")
@@ -148,10 +176,12 @@ if __name__ == "__main__":
             hidden = False
         print("Game name:")
         game_name = input()
-        game_id = createGame(url, game_id, player_id, hidden)
+        max_players = getIntInput("Max Players: ", 1, 8)
+        game_id, session_id = createGame(url, game_name,
+                player_id, hidden, max_players)
     elif g == "2":
         g = input()
-        if g in [row["game_id"] for row in getGames(url)]
+        if g in [row["game_id"] for row in getGames(url)]:
             game_id = g
         else:
             print("failed to query requested game")
@@ -163,7 +193,7 @@ if __name__ == "__main__":
             print("failed to get game list")
             exit(1)
         for game in range(len(games)):
-            print(game + ": " + games[game])
+            print(str(game) + ": " + games[game]["game_name"])
 
         print("Select a game: ")
         g = input()
@@ -176,7 +206,9 @@ if __name__ == "__main__":
     # game_id set, attempt join
     print("Enter password:")
     password = input()
-    if joinGame(url, game_id, player_id, password) == False:
+    if bool(session_id) == False:
+        session_id = joinGame(url, game_id, player_id, password)
+    if session_id == False:
         print("Failed to join game")
         exit(1)
 
@@ -185,66 +217,93 @@ if __name__ == "__main__":
     while quit == False:
         
         print("Waiting for game to begin...")
-        qry = qryGame(url, game_id, player_id)
+        qry = qryGame(url, session_id)
+        if bool(qry) == False:
+            print("Initial query failed.")
+            exit(1)
         while State(qry["state"]) == State.LOBBY:
-            qry = qryGame(url, game_id, player_id)
             print("Waiting for game start.")
             print("Players: ",
                     (str(qry["player_count"]) + "/" + str(qry["max_players"])),
                     sep="")
-            for p in qry["players"].keys()
+            for p in qry["players"].keys():
                 print(qry["players"][p])
-            if qry["host"] = player_id:
-                print("1: Refresh players")
+            if qry["host"] == player_id:
+                print("1: Refresh")
                 print("2: Start game")
                 i = getIntInput("?", 1, 2)
                 if i == 2:
-                    startGame(url, game_id, player_id)
+                    strtgame = startGame(url, session_id)
+                    if strtgame != True:
+                        print(strtgame)
 
             else:
                 time.sleep(2)
 
+            qry = qryGame(url, session_id)
+
         print("Game has started.")
-        qry = qryGame(url, game_id, player_id)
-        prompt = qry["current_prompt"]
+        qry = qryGame(url, session_id)
+        call = unpackCardText(qry["current_prompt"])
         cards_req = qry["cards_req"]
         cards = qry["cards"]
         czar = qry["czar"]
         cards_submitted = 0
         sub = []
-        while cards_submitted < cards_req:
-            print("Czar: ", czar, sep="")
-            print("Prompt: ", prompt, sep="")
+        sub_text = []
+        if czar != player_id:
+            while cards_submitted < cards_req:
+                print("Czar: ", qry["players"][czar], sep="")
+                print("Prompt: ", call, sep="")
 
-            print("-------------------------")
-            
-            print("Current submission:")
-            for c in range(len(sub)):
-                print(str(c) + sub[c])
+                print("-------------------------")
+                
+                print("Current submission (" + 
+                        str(cards_submitted) + "/" + str(cards_req) + "):")
+                for c in range(len(sub_text)):
+                    print(str(c) + ": " + sub_text[c])
 
-            print("-------------------------")
+                print("-------------------------")
 
-            print("Your cards:")
-            for c in range(len(cards)):
-                print(str(c) + ' ' + unpackCardText(cards[c]["text"]))
+                print("Your cards:")
+                for c in range(len(cards)):
+                    print(str(c + 1) + '. ' + unpackCardText(cards[c]["text"]))
 
-            c = getIntInput("Submit a card:",
-                    1, len(cards) + 1) - 1
-            sub.append(cards[c]["id"])
+                c = getIntInput("Submit a card:",
+                        1, len(cards) + 1) - 1
+                sub.append(cards[c]["response_id"])
+                sub_text.append(unpackCardText(cards[c]["text"]))
+                del cards[c]
+                cards_submitted += 1
 
-        submit(url, game_id, player_id, sub)
+            submit(url, session_id, sub)
 
+        print("Waiting for submissions...")
         while State(qry["state"]) == State.PROMPT:
-            qry = qryGame(url, game_id, player_id)
+            qry = qryGame(url, session_id)
             time.sleep(2)
 
         print("Submission stage complete")
 
-        print("Waiting for " + qry["czar"] + " to pick a favorite...")
+        print("Waiting for ", qry["players"][czar], 
+                " to pick a favorite...", sep="")
         while State(qry["state"]) == State.SELECT:
-            qry = qryGame(url, game_id, player_id)
             if qry["czar"] == player_id:
-                # pick a favorite
-                pass
+                subs = qry["subs"]
+                input_prompt = ""
+                for n in range(len(subs)):
+                    sub = subs[n]
+                    sub_player_id = sub["player_id"]
+                    sub_player_name = qry["players"][sub_player_id]
+                    input_prompt += str(n + 1) + ". " + sub_player_name + ":\n"
+                    for m in len(sub["cards"]):
+                        card = sub["cards"][m]
+                        input_prompt += "\n" + unpackCardText(card["text"]) + "\n"
+
+                sel_input = getIntInput(1, len(subs)) - 1
+                sel_id = subs[sel_input]["player_id"]
+                select(sel_id)
             else:
                 time.sleep(2)
+            qry = qryGame(url, session_id)
+
--- pip-installs.sh
+++ pip-installs.sh
@@ -1,2 +1,3 @@
 pip install --upgrade pip
+pip install --upgrade wheel
 pip install --upgrade requests
